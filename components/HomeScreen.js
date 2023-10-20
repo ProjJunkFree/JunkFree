07a20b856a538e35ab0f4638cc015e53e4433368
Jie clark Terec <jieclarkm@gmail.com>
2023-10-20 11:51:58 +0800

    Terec - Fixed navigations

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, SafeAreaView, ScrollView, Text, Button } from "react-native";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+
+// component
 
 function HomeScreen({ navigation }) {
   const handleProfile = () => {
@@ -8,11 +12,25 @@ function HomeScreen({ navigation }) {
   };
 
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Button title="Go to Profile" onPress={handleProfile} />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: COLORS.red,
+            padding: SIZES.medium,
+          }}
+        >
+          <Text>Home Screen</Text>
+          <Button title="Go to Profile" onPress={handleProfile} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+HomeScreen.navigationOptions = {
+  headerShown: false,
+};
 
 export default HomeScreen;
