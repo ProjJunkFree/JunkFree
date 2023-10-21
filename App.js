32a3f21d9ea21a0fe63e9e7a29be4e54ddbff6b3
skhandayan <handayan.seankirk1@gmail.com>
2023-10-21 16:40:58 +0800

    Added Forgot Password Page and some Validations - Handayan

--- App.js
+++ App.js
@@ -11,6 +11,9 @@ import HomeScreen from "./components/HomeScreen";
 import ProfileScreen from "./components/ProfileScreen";
 import { StyleSheet, View, Text } from "react-native";
 import LoginScreen from "./components/LoginScreen";
+import ForgotPasswordScreen from "./components/ForgotPasswordScreen/ForgotPasswordScreen";
+import ResetPasswordScreen from "./components/ForgotPasswordScreen/ResetPasswordScreen";
+import ConfirmationCodeScreenScreen from "./components/ForgotPasswordScreen/ConfirmationCodeScreen";
 
 export default function App() {
   return (
@@ -18,6 +21,9 @@ export default function App() {
       <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Forgot Password" component={ForgotPasswordScreen} />
+        <Stack.Screen name="Confirmation Code" component={ConfirmationCodeScreenScreen} />
+        <Stack.Screen name="Reset Password" component={ResetPasswordScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
